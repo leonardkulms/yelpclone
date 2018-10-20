e8c1019689c5083372ff6b738f570c84c73833d0
Leonard Kulms <>
2018-10-20 23:37:26 +0200

    refactoring routes

---
+++ routes/comments.js
@@ -0,0 +1,50 @@
+const express   = require("express");
+const router    = express.Router({mergeParams:true});
+const Campground = require("../models/campground");
+const Comment = require("../models/comment")
+
+// ====================
+// COMMENTS ROUTES
+// ====================
+
+//COMMENTS NEW
+
+router.get("/new", isLoggedIn, function(req, res){
+    // find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+             res.render("comments/new", {campground: campground});
+        }
+    })
+});
+
+//COMMENTS CREATE
+router.post("/", isLoggedIn, function(req, res){
+   //lookup campground using ID
+   Campground.findById(req.params.id, function(err, campground){
+       if(err){
+           console.log(err);
+           res.redirect("/campgrounds");
+       } else {
+        Comment.create(req.body.comment, function(err, comment){
+           if(err){
+               console.log(err);
+           } else {
+               campground.comments.push(comment);
+               campground.save();
+               res.redirect('/campgrounds/' + campground._id);
+           }
+        });
+       }
+   });
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return(next());
+    }
+    res.redirect("/login");
+    }
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,57 @@
+const express   = require("express");
+const router    = express.Router();
+const passport  = require("passport");
+const User      = require("../models/user");
+
+// LANDING
+router.get("/", function(req, res){
+    res.render("landing");
+});
+
+// AUTH ROUTES
+
+// show register form
+router.get("/register", function(req, res){
+    res.render("register");
+});
+
+
+// handle signing up
+router.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
+    });
+});
+
+//show login form
+router.get("/login", function(req,res){
+    res.render("login");
+});
+
+
+//handle login logic
+router.post("/login", passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+}), function(req, res){  
+});
+
+//log out
+router.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return(next());
+    }
+    res.redirect("/login");
+    }
+module.exports = router;
