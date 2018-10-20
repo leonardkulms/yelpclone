45daae491966f8bd463d9080c4d57d014750256f
Leonard Kulms <>
2018-10-21 00:48:46 +0200

    association: objects and comments with users

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose.connect("mongodb://localhost/yelp_camp_v4");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-seedDB();
+// seedDB();
 
 //========= PASSPORT CONFIG
 app.use(require("express-session")({
--- models/campground.js
+++ models/campground.js
@@ -4,6 +4,13 @@ var campgroundSchema = new mongoose.Schema({
    name: String,
    image: String,
    description: String,
+   author:{
+       id: {
+           type: mongoose.Schema.ObjectId,
+           ref: "User"
+       },
+       name: String
+   },
    comments: [
       {
          type: mongoose.Schema.Types.ObjectId,
